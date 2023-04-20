3fe938530f6ffe7f833149de68895f91de48715f
Keandrrre <k.kuzniar@share.epsb.ca>
2023-04-20 14:47:49 -0600

    Sort...

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # Data Management Project/ Music Playlist
 import os
+import json
 
 songs = [{'title': 'Jungle', 'artist': 'Drake'},
-         {'title': 'Replay', 'artist': 'Tems'}]
+         {'title': 'Replay', 'artist': 'Tems'}, {'title': 'Search & Rescue', 'artist': 'Drake'}, {'title': 'Kill Bill', 'artist': 'SZA'}, {'title': "Creepin'", 'artist': 'Metro Boomin, The Weekend & 21 Savage'}, {'title': 'Rich Flex', 'artist': 'Drake & 21 Savage'}]
 
 # User Options
 loop = True
@@ -11,41 +12,56 @@ while loop == True:
         "1-Display\n2-Filter Data\n3-Sort Data\n4-Add Data to Favourites\n5-Remove Data from Favourites\n6-Display Favourites\n\nInput: "))
     if user_selection == 1:
         os.system('cls')
-        print("Displaying All Songs:")
-
+        print("DISPLAY ALL")
         # Display all Songs
         for i in range(len(songs)):
-            print('\t' + songs[i]['title'] +
+            print(songs[i]['title'] +
                   ' (By ' + songs[i]['artist'] + ')')
         loop = False
     elif user_selection == 2:
         os.system('cls')
-        print("Filter")
-
+        print("FILTER")
         # Filter songs
         filter_by = int(
-            input('1-By Title\n2-By Artist\n3-By Genre\nEnter Here:'))
+            input('1-By Title\n2-By Artist\n3-By Genre\n\nEnter Here:'))
+        os.system('cls')
         if filter_by == 1:
             filter_data = input('Enter Title: ')
+            os.system('cls')
+            print('DISPLAYING SONGS TITLED "' + str(filter_data) + '"')
             for i in range(len(songs)):
-                if songs[i]['title'] == filter_data:
-                    print('\t' + songs[i]['title'] +
+                if filter_data.lower() in songs[i]['title'].lower():
+                    print(songs[i]['title'] +
                           ' (By ' + songs[i]['artist'] + ')')
         elif filter_by == 2:
             filter_data = input('Enter Artist: ')
+            os.system('cls')
+            print('DISPLAYING SONGS BY "' + str(filter_data) + '"')
             for i in range(len(songs)):
-                if songs[i]['artist'] == filter_data:
-                    print('\t' + songs[i]['title'] +
+                if filter_data.lower() in songs[i]['artist'].lower():
+                    print(songs[i]['title'] +
                           ' (By ' + songs[i]['artist'] + ')')
         elif filter_by == 3:
             filter_data = input('Enter Genre: ')
+            os.system('cls')
+            print('DISPLAYING SONGS UNDER "' + str(filter_data) + '"')
             for i in range(len(songs)):
-                if songs[i]['genre'] == filter_data:
-                    print('\t' + songs[i]['title'] +
+                if filter_data.lower() in songs[i]['genre'].lower():
+                    print(songs[i]['title'] +
                           ' (By ' + songs[i]['artist'] + ')')
         loop = False
     elif user_selection == 3:
-        print("Sort")
+        os.system('cls')
+        print("SORT")
+        # Sort Songs
+        sort_by = int(input('1-Title\n2-Artist\n3-Genre\n\nEnter Here: '))
+        os.system('cls')
+        if sort_by == 1:
+            songs.sort(key=songs['title'])
+            for i in range(len(songs)):
+                print(songs[i]['title'] +
+                      ' (By ' + songs[i]['artist'] + ')')
+        loop = False
     elif user_selection == 4:
         print("Add to Favourites")
     elif user_selection == 5:
